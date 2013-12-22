bbb0256a3410b36cd7983b8d1e3b252c803f885b
asmallorange <jeanalesi007@gmail.com>
2013-12-22 08:18:17 -0300

    changing form design.. adding divs to display errors

--- views/v_users_login.php
+++ views/v_users_login.php
@@ -1,23 +1,25 @@
 <div id="wrapper">
 
-	<form name="login-form" class="login-form" action="/users/p_login" method="POST">
+	<form name="login-form" class="login-form">
 	
 		<div class="header">
 		<h1>Login</h1>
+		<div id="response"></div>
 		</div>
 	
 		<div class="content">
-		<input name="username" type="text" class="input username" placeholder="Email" />
+		<input name="username" type="text" class="input username" placeholder="Username" id="username" />
 		<div class="user-icon"></div>
-		<input name="password" type="password" class="input password" placeholder="Password" />
+		<input name="password" type="password" class="input password" placeholder="Password" id="password" />
 		<div class="pass-icon"></div>		
 		</div>
 
 		<div class="footer">
-		<input type="submit" name="submit" value="Login" class="button" />
-		<input type="submit" name="submit" value="Register" class="register" />
+		<a class="button" id="p_login">Login</a>
+		<a href="/users/signup" class="register">Register</a>
 		</div>
 	
 	</form>
+	
 
 </div>
