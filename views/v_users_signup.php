955e5914d9671c50466f192bf43e420b139f8c68
asmallorange <jeanalesi007@gmail.com>
2013-12-22 08:17:59 -0300

    changing form design

--- views/v_users_signup.php
+++ views/v_users_signup.php
@@ -1,70 +1,29 @@
-<h4>signup</h4>
+<div id="wrapper">
 
-        <link rel='stylesheet' href='popbox.css' type='text/css'>
+	<form name="login-form" class="login-form">
+	
+		<div class="header">
+			<h1>Sign up</h1>
+			<div id="fields_error"></div>
+			<div id="username_error"></div>
+			<div id="password_error"></div>
+		</div>
+	
+		<div class="content">
+		<input name="username" type="text" class="input username" placeholder="Email" name="username" id="username"/>
+		<div class="user-icon"></div>
+		<input name="password" type="password" class="input password" placeholder="Password" name="password" id="password" />
+		<div class="pass-icon"></div>
+		<div class="user-icon"></div>
+		<input name="password" type="password" class="input password" placeholder="Password" name="password2" id="password2" />
+		<div class="pass-icon"></div>		
+		</div>
 
-        <div class='popbox'>
-          <a class='open' href='#'>Click Here!</a>
+		<div class="footer">
+		<a class="button" id="p_signup">Sign Up</a>
+		<a href="/users/login" class="register" >Login</a>
+		</div>
+	
+	</form>
 
-          <div class='collapse'>
-            <div class='box'>
-              <div class='arrow'></div>
-              <div class='arrow-border'></div>
-
-              Content in PopBox goes here :)
-              
-								<div id="stylized" class="myform">
-								
-								<!-- whichever variable gets passed as TRUE into the template, enables the corresponding "if" condition to
-								allow execution... thus, echoing error messages in accord to the situation. -->
-					
-									<form id="form" name="form" action='/users/p_signup' method="POST">
-										<h1>Sign-up</h1>
-										<p>Sign up to SpeedTypr Reverse! <a id="login_link" href="/users/login">Login</a></p>
-								
-										<label>Username
-										<span class="small">Choose a username</span>
-										</label>
-										<input type="text" name="username" id="username" />
-								
-										<label>Password
-										<span class="small">Min. size 6 chars</span>
-										</label>
-										<input type="password" name="password" id="password" />
-								
-										<label>Password
-										<span class="small">Re-enter your password</span>
-										</label>
-										<input type="password" name="password2" id="password2" />
-								
-										<button type="submit">Sign-up</button>
-										<div class="spacer"></div>
-									</form>
-								</div>
-              
-              
-              <a href="#" class="close">close</a>
-            </div>
-          </div>
-        </div>
-
-        
-        <script type='text/javascript'>
-           $(document).ready(function(){
-             $('.popbox').popbox();
-           });
-        </script>
-        
-        
-		<script>
-	/*		var options = {
-					
-					type: "POST",
-					url: "/users/p",
-					success: function(response) {
-						$("#response").html(response);
-						console.log(response);
-					}
-			}
-		
-			$("form").ajaxForm(options);     */
-        </script>
+</div>
