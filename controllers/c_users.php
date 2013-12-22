31a709a08b2ae616b36638207e53f9a0c48df2c0
asmallorange <jeanalesi007@gmail.com>
2013-12-22 08:15:31 -0300

    adjusting for javascript functionality, json_encode -ing.

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -21,7 +21,7 @@ class users_controller extends base_controller
 		echo $this->template;
 	}
 	
-	public function signup($username = NULL, $password = NULL, $empty_field = NULL)
+	public function signup()
 	{
 		# Make sure this page cannot be accessed when the user is logged in
 		if ($this->user)
@@ -31,10 +31,15 @@ class users_controller extends base_controller
 		$this->template->content = View::instance("v_users_signup");
 	
 		# Set up header files
-		$client_files_head = array("/css/style.css", "/css/popbox.css", "/js/popbox.min.js");
+		$client_files_head = array("/css/style.css", '/css/users_login.css');
 		$this->template->client_files_head = Utils::load_client_files($client_files_head);
 		
+		# Set client files in body
+		$client_files_body = array('/js/signup.js');
+		$this->template->client_files_body = Utils::load_client_files($client_files_body);
+		
 		# Set title
+		$this->template->title = 'Sign Up';
 		
 		# Pass info to View
 		
@@ -44,29 +49,60 @@ class users_controller extends base_controller
 	
 	public function p_signup()
 	{
+		// Prepare data array
+		$data = Array();	
+		
 		#Sanitize
 		$_POST = DB::instance(DB_NAME)->sanitize($_POST);
 	
 		#Check to see if the username has already been chosen. Usernames must be UNIQUE in the database. If the username is found, set variable to true
 		$username_exists = DB::instance(DB_NAME)->select_field("SELECT username FROM users WHERE username = '".$_POST['username']."'");
 		if ($username_exists)
-			$username_exists = true;
+		{
+			$username_exists = TRUE;
+			$data['username_error'] = 'Username already exists.';
+		}
+		
+		$password_error = FALSE;					// holds until tested wrong
 		
 		#check for password match
-		if( $_POST['password'] != $_POST['password2'] || strlen($_POST['password']) < 6)
-			$password_error = true;
-		else $password_error = false;
+		if ($_POST['password'] != $_POST['password2'])
+		{
+			$password_error = TRUE;
+			$data['password_error'] = 'Password missmatch.';
+		}
 		
+		# Ensure password length is at least 6 chars
+		if (strlen($_POST['password']) < 6)
+		{
+			if (isset($data['password_error']))
+			{
+				$password_error = TRUE;
+				$data['password_error'] = $data['password_error'].' Password too short';
+			}
+			else 
+			{
+				$password_error = TRUE;
+				$data['password_error'] = 'Password too short';
+			}
+		}
+		
+		$empty_field = FALSE;
 		#check for empty fields
-		$empty_field = false;
 		foreach ($_POST as $key => $value)
 		{
 			if ( strlen($value) < 1 )
-				$empty_field = true;
+			{
+				$empty_field = TRUE;
+				$data['empty_fields_error'] = "All fields are required.";
+			}
 			
 		#check for fields with only the blank character
 			if (ltrim($value, ' ') === '')
-				$empty_field = true;
+			{
+				$empty_field = TRUE;
+				$data['empty_fields_error'] = "All fields are required.";
+			}
 		}
 
 		#Update the post data in order to inject into database
@@ -81,12 +117,15 @@ class users_controller extends base_controller
 			DB::instance(DB_NAME)->insert_row('users', $_POST);
 			$token = $_POST['token'];
 			setcookie('token', $token, strtotime('+1 week'), '/');
-			Router::redirect('/users/index');
+			$data['success'] = TRUE;
 		}
 		
 		#There are errors, display the signup page again, returning the cause
 		else
-			$this->signup($username_exists, $password_error, $empty_field);
+		{
+			$data['success'] = FALSE;
+		}
+		echo json_encode($data);
 	}
 	
 	public function login() {
@@ -101,18 +140,22 @@ class users_controller extends base_controller
 		$this->template->title = "Login";
 	
 		#Set header information
-//		$client_files_head = array('/css/users_login.css');
-//		$this->template->client_files_head = Utils::load_client_files($client_files_head);
+		$client_files_head = array('/css/users_login.css');
+		$this->template->client_files_head = Utils::load_client_files($client_files_head);
+		
+		#Add files to body
+		$client_files_body = array('/js/login.js');
+		$this->template->client_files_body = Utils::load_client_files($client_files_body);
 		
 		#Display the view
 		echo $this->template;
-		
 	}
 	
 	public function p_login() {
-		#If the user clicked on the register link, redirect...
-		if ($_POST['submit'] == "Register")
-			Router::redirect('/users/signup');
+		// Prepare our response array.
+		$data = Array();
+		// Sanitize
+		$_POST = DB::instance(DB_NAME)->sanitize($_POST);
 		
 		#Encrypt
 		$_POST['password'] = sha1(PASSWORD_SALT.$_POST['password']);
@@ -124,13 +167,16 @@ class users_controller extends base_controller
 		#Success
 		if($token)
 		{
-		setcookie('token', $token, strtotime('+1 week'), '/');
-		Router::redirect('./index');
+			setcookie('token', $token, strtotime('+1 week'), '/');
+			$data['success'] = TRUE;
 		}
 		else
 		{
-		echo "Login Failed!";
-		}
+			$data['error'] = 'Login Failed';
+			$data['success'] = FALSE;
+		}	
+		echo json_encode($data);
+		
 	}
 	
 	public function logout() {
