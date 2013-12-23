71b39d3ed3e0ae19e47e54ab863fd931cfbeef02
asmallorange <jeanalesi007@gmail.com>
2013-12-23 03:19:34 -0300

    added classes and extra divs to create css effects

--- views/v_decision_tree.php
+++ views/v_decision_tree.php
@@ -1,23 +1,29 @@
 
-<h3 id=""><?php echo $tree_title;?></h3>
+<h3 id="tree_title"><?php echo $tree_title;?></h3>
 <div id="username" <?php echo "data-username='".$username."'";?>></div>
 <div id="tree_name" <?php echo "data-tree_name='".$tree_name."'";?>></div>
 
+<div id="form_div">
 <a href="javascript:history.go(0)"id='restart'>Restart</a>
-<form>
+<form id="radio_form">
 <input id="yes" type="radio" name="path" value='1' checked>Yes
 <input id="no" type="radio" name="path" value='0' disabled>No
 <input type="submit">
-
-<p id="response0"></p>
-<p id="response1"></p>
-<p id="response2"></p>
-<p id="response3"></p>
-<p id="response4"></p>
-<p id="response5"></p>
-<p id="response6"></p>
-<p id="response7"></p>
-<p id="response8"></p>
-<p id="response9"></p>
-<p id="response10"></p>
 </form>
+</div>
+
+<div id="responses">
+
+	<p class="branches" id="response0"></p>
+	<p class="branches" id="response1"></p>
+	<p class="branches" id="response2"></p>
+	<p class="branches" id="response3"></p>
+	<p class="branches" id="response4"></p>
+	<p class="branches" id="response5"></p>
+	<p class="branches" id="response6"></p>
+	<p class="branches" id="response7"></p>
+	<p class="branches" id="response8"></p>
+	<p class="branches" id="response9"></p>
+	<p class="branches" id="response10"></p>
+
+</div>
