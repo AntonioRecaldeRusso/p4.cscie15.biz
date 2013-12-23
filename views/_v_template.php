3dd4c9043a328570054ad5d26678f98d21527c2c
asmallorange <jeanalesi007@gmail.com>
2013-12-23 04:10:47 -0300

    added navAccess.js functionality.. switcheroo of <a> tag placements

--- views/_v_template.php
+++ views/_v_template.php
@@ -8,6 +8,7 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
 	<script src="/js/jquery.form.js"></script>
+	<script src="/js/navAccess.js"></script>
 	<link rel="stylesheet" type="text/css" href="/css/style.css">
 	
 					
@@ -24,10 +25,12 @@
 		<h1 id="header"><span id="offset_balancer"><?php if (isset($user->username)) echo $user->username; ?></span>DecisionTree<a id="user_logged"><?php if (isset($user->username)) echo $user->username; ?></a></h1> <br>
 			<div id="navbar"> 
 		  	<ul> 
-			<li><a href="/users/login">Login</a></li>
-			<li><a href="/users/logout">Logout</a></li>
+			<li><a class="logged_off" href="/users/login">Login</a></li>
+			<li><a class="logged_off" href="/users/signup">Signup</a></li>
 			<li><a href="/decision/index">Trees</a></li>
-			<li><a href="/users/signup">Signup</a></li>
+			<li><a href="/decision/create">Create</a></li>
+			<li><a class="logged_in" href="/users/logout">Logout</a></li>
+			
 		  	</ul> 
 		</div>
 	</div>
