c6784b4941ed1e0692c7e6ab7a169f746fc66660
asmallorange <jeanalesi007@gmail.com>
2013-12-23 07:54:50 -0300

    Pasted tournament bracket. Added text fields.

---
+++ views/v_decision_create.php
@@ -0,0 +1,70 @@
+<p>Instructions: NORTH = YES, SOUTH = NO. Binary numbers symbolize y/n. 1111 = yes yes yes.. 1000 = no no no. The first character is '1' by default. In order to link one answer with another, use @ followed by the binary code that corresponds to the answer.. E.g. If at 100 I type @111, that space in the bracket will become 111.. Thus, if the next answer is "yes," the following index will be 1111, and not 1001 (which is what it would have been without the linking). This is a tree, therefore the branching pattern should be intuitive.</p>
+<form>
+
+<table summary="Tournament Bracket">
+ <tr>
+  <td><p><label>11111</label><input type="text" size="24"/></p></td>
+  <td rowspan="2"><p><label>1111</label><input type="text" size="24"/></p></td>
+  <td rowspan="4"><p><label>111</label><input type="text" size="24"/></p></td>
+  <td rowspan="8"><p><label>11</label><input type="text" size="24"/></p></td>
+  <td rowspan="16"><p><label>1</label><input type="text" size="24"/></p></td>
+ </tr>
+ <tr>
+  <td><p><label>11110</label><input type="text" size="24"/></p></td>
+ </tr>
+ <tr>
+  <td><p><label>11101</label><input type="text" size="24"/></p></td>
+  <td rowspan="2"><p><label>1110</label><input type="text" size="24"/></p></td>
+ </tr>
+ <tr>
+  <td><p><label>11100</label><input type="text" size="24"/></p></td>
+ </tr>
+ <tr>
+  <td><p><label>11011</label><input type="text" size="24"/></p></td>
+  <td rowspan="2"><p><label>1101</label><input type="text" size="24"/></p></td>
+  <td rowspan="4"><p><label>110</label><input type="text" size="24"/></p></td>
+ </tr>
+ <tr>
+  <td><p><label>11010</label><input type="text" size="24"/></p></td>
+ </tr>
+ <tr>
+  <td><p><label>11001</label><input type="text" size="24"/></p></td>
+  <td rowspan="2"><p><label>1100</label><input type="text" size="24"/></p></td>
+ </tr>
+ <tr>
+  <td><p><label>11000</label><input type="text" size="24"/></p></td>
+ </tr>
+ <tr>
+  <td><p><label>10111</label><input type="text" size="24"/></p></td>
+  <td rowspan="2"><p><label>1011</label><input type="text" size="24"/></p></td>
+  <td rowspan="4"><p><label>101</label><input type="text" size="24"/></p></td>
+  <td rowspan="8"><p><label>10</label><input type="text" size="24"/></p></td>
+ </tr>
+ <tr>
+  <td><p><label>10110</label><input type="text" size="24"/></p></td>
+ </tr>
+ <tr>
+  <td><p><label>10101</label><input type="text" size="24"/></p></td>
+  <td rowspan="2"><p><label>1010</label><input type="text" size="24"/></p></td>
+ </tr>
+ <tr>
+  <td><p> <label>10100</label><input type="text" size="24"/></p></td>
+ </tr>
+ <tr>
+  <td><p> <label>10011</label><input type="text" size="24"/></p></td>
+  <td rowspan="2"><p><label>1001</label><input type="text" size="24"/></p></td>
+  <td rowspan="4"><p><label>100</label><input type="text" size="24"/></p></td>
+ </tr>
+ <tr>
+  <td><p><label>10010</label><input type="text" size="24"/></p></td>
+ </tr>
+ <tr>
+  <td><p><label>10001</label><input type="text" size="24"/></p></td>
+  <td rowspan="2"><p><label>1000</label><input type="text" size="24"/></p></td>
+ </tr>
+ <tr>
+  <td><p><label>10000</label><input type="text" size="24"/></p></td>
+ </tr>
+</table>
+
+</form>
