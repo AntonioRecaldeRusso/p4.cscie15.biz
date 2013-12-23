5f16035bdcb632bc0b5cc88b8087e9f55e19ed84
asmallorange <jeanalesi007@gmail.com>
2013-12-23 07:52:37 -0300

    updated title,  data placeholder for js -> php

--- views/v_decision_tree.php
+++ views/v_decision_tree.php
@@ -1,10 +1,11 @@
 
 <h3 id="tree_title"><?php echo $tree_title;?></h3>
-<div id="username" <?php echo "data-username='".$username."'";?>></div>
-<div id="tree_name" <?php echo "data-tree_name='".$tree_name."'";?>></div>
+<div id="username" <?php echo "data-username='".$username."'";?>></div><div id="tree_name" <?php echo "data-tree_name='".$tree_name."'";?>></div>
+
+<p id="instructions">Click "Send" to start. Select Yes/No and get your answer</p>
 
 <div id="form_div">
-<a href="javascript:history.go(0)"id='restart'>Restart</a>
+<a href="javascript:history.go(0)" id='restart'>Restart</a>
 <form id="radio_form">
 <input id="yes" type="radio" name="path" value='1' checked>Yes
 <input id="no" type="radio" name="path" value='0' disabled>No
