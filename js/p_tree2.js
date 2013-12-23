991de8825800ca507564b1c7d603536ce72cd554
asmallorange <jeanalesi007@gmail.com>
2013-12-23 03:18:18 -0300

    used html data to convey values to php file
    I was not able to pass dynamically updated values from Ajax to css..
    hence instead I updated a value inside a div, and passed that to the php
    file.

--- js/p_tree2.js
+++ js/p_tree2.js
@@ -1,15 +1,7 @@
 	counter = 0;
 	index = '';
-	path = '';
-	username = '';
-	tree_name = '';
-	
-	$(function() {
-		username = $('#username').data("username");
-		tree_name = $('#tree_name').data("tree_name");
-	});
-	
-	
+	last_message = '';
+
 	var options = {
 			type: "POST",
 			url: "/decision/p_tree2/",
@@ -20,27 +12,40 @@
 					$('#no').attr('disabled', false);
 				
 				if (id == 'yes')
+				{
+					$('#response' + (counter - 1)).css('background-color', 'lime');
 					index = index.concat('1');
+				}
+					
 				else
+				{
+					$('#response' + (counter - 1)).css('background-color', 'red');
 					index = index.concat('0');
+				}
 			},
 			data: {
-				username: username,
-				tree_name: tree_name
+				username: $('#username').data("username"),
+				tree_name: $('#tree_name').data("tree_name")
 			},
 			success: function(response) {
-			var data = $.parseJSON(response);
+				var data = $.parseJSON(response);
 				 
+				if (data['end'] == true)
+				{
+					$('#response' + (counter - 1)).css('background-color', 'yellow');
+					alert("End of branch.\n\n" + last_message);
+				}
+				
 				if (data['link'] != null)
-					{
 						index = data['link'];
-					}
+					
 				
 				$("#yes").val('' + index + '1');
 				$('#no').val('' + index + '0');
 				$('#response' + counter++).html(data['content']);
+				last_message = data['content'];  
+				console.log(data);
 			}
-			
 	};
 
 	$("form").ajaxForm(options);
