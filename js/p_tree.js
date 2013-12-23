069de27c0aafddb5d9ee4ae3c929264799e3bbbc
asmallorange <jeanalesi007@gmail.com>
2013-12-23 04:08:52 -0300

    switched p_tree2.js to p_tree.js

--- js/p_tree.js
+++ js/p_tree.js
@@ -1,9 +1,10 @@
 	counter = 0;
 	index = '';
-	path = '';
-	
+	last_message = '';
+
 	var options = {
 			type: "POST",
+			url: "/decision/p_tree/",
 			beforeSend: function() {
 				id = $('input[name="path"]:checked').attr('id');
 				
@@ -11,22 +12,40 @@
 					$('#no').attr('disabled', false);
 				
 				if (id == 'yes')
+				{
+					$('#response' + (counter - 1)).css('background-color', 'lime');
 					index = index.concat('1');
+				}
+					
 				else
+				{
+					$('#response' + (counter - 1)).css('background-color', 'red');
 					index = index.concat('0');
-			},	
-			url: "/decision/p_tree/",
-	//		data: {
-	//			index: index 
-	//		},
+				}
+			},
+			data: {
+				username: $('#username').data("username"),
+				tree_name: $('#tree_name').data("tree_name")
+			},
 			success: function(response) {
-				console.log(response);
+				var data = $.parseJSON(response);
+				 
+				if (data['end'] == true)
+				{
+					$('#response' + (counter - 1)).css('background-color', 'yellow');
+					alert("End of branch.\n\n" + last_message);
+				}
+				
+				if (data['link'] != null)
+						index = data['link'];
+					
+				
 				$("#yes").val('' + index + '1');
 				$('#no').val('' + index + '0');
-				$('#response' + counter++).html(response);
-				
+				$('#response' + counter++).html(data['content']);
+				last_message = data['content'];  
+				console.log(data);
 			}
-			
 	};
 
 	$("form").ajaxForm(options);
